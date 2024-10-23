e68f4280eafa1eaa37ecb1142801847149f5b456
rizqikevino <rizqikevino@gmail.com>
2024-10-23 13:46:56 +0700

    first commit

---
+++ bot-game/bot.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Nesk\Puphpeteer\Puppeteer;
+
+$puppeteer = new Puppeteer();
+$browser = $puppeteer->launch(['headless' => false]);
+$page = $browser->newPage();
+
+try {
+    $page->goto('https://farmrpg.com/index.php#!/login', [
+        'waitUntil' => 'networkidle0',
+    ]);
+
+    $page->waitForSelector('#username');
+    $page->type('#username', 'username_kamu'); 
+    $page->type('#password', 'password_kamu'); 
+    
+    $page->click('#submit-button'); 
+    
+
+    $page->waitForNavigation(['waitUntil' => 'networkidle0']);
+
+    echo "Login berhasil! Memulai permainan...\n";
+
+    $page->goto('https://farmrpg.com/game');
+    echo "Permainan dimulai...\n";
+
+    $page->click('.start-button-selector');
+
+    echo "Permainan telah dimulai...\n";
+
+} catch (\Exception $e) {
+    echo "Terjadi kesalahan: " . $e->getMessage();
+} finally {
+    $browser->close();
+}
